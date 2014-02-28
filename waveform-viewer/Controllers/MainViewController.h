5041ff916edbe70545099ade686e9a6eb902d56a
daniel <baniel.decker@gmail.com>
2014-02-28 10:28:06 +0100

    Added empty storyboard and MainViewController

---
+++ waveform-viewer/Controllers/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  waveform-viewer
+//
+//  Created by Daniel Becker on 28.02.14.
+//  Copyright (c) 2014 Uni Kassel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
